9f6dea6e7b682c58493f2d0906baa10fc6a6abd0
Matthew Sullivan <elephant454@gmail.com>
2015-06-30 22:22:28 -0700

    added letterFind method

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <stdlib.h>
-#include <string.h>
 #include <string>
 
 using namespace std;
@@ -22,8 +20,29 @@ string randomWord(){
 	return word;
 }
 
+int letterFill(string guess, string secretword, string &guessword){
+    int i;
+
+    int matches=0;
+
+    int len=secretword.length();
+
+    for (i = 0; i< len; i++)
+    {
+
+        // Did we already match this letter in a previous guess?
+        if (guess == guessword[i]) return 0;
+
+    // Is the guess in the secret word?
+        if (guess == secretword[i]){
+            guessword[i] = guess;
+            matches++;
+        }
+    }
+    return matches;
+}
+
 int main(){
-	int letterFill(string, string, string&);
 	int TRIES = 0;
 	int MAX_TRIES = 5;
 	string letter = "";
@@ -61,3 +80,4 @@ int main(){
 
 	return 0;
 }
+
