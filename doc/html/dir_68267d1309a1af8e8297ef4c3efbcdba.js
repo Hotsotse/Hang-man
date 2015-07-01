55421daea72fe1d50294fbe9446de5eb852b02c4
Matthew Sullivan <elephant454@gmail.com>
2015-07-01 12:49:08 -0700

    a char is taken for letter guesses as opposed to a string. It finally compiles. Tweaked makefile, added doxygen stuff. Should have made more commits in-between.

---
+++ doc/html/dir_68267d1309a1af8e8297ef4c3efbcdba.js
@@ -0,0 +1,4 @@
+var dir_68267d1309a1af8e8297ef4c3efbcdba =
+[
+    [ "main.cpp", "main_8cpp_source.html", null ]
+];
---
+++ doc/html/navtreedata.js
@@ -0,0 +1,16 @@
+var NAVTREE =
+[
+  [ "Hang-man", "index.html", [
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"dir_68267d1309a1af8e8297ef4c3efbcdba.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
