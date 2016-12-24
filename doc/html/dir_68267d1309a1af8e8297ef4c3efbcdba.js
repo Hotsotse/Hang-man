4231d0cb3eb35a1ab1b8f116aeefecadcf191cdb
Matthew Sullivan <elephant454@gmail.com>
2016-12-24 13:17:06 -0800

    removed html and latex documentation, as it can be generated locally

--- doc/html/dir_68267d1309a1af8e8297ef4c3efbcdba.js
+++
@@ -1,4 +0,0 @@
-var dir_68267d1309a1af8e8297ef4c3efbcdba =
-[
-    [ "main.cpp", "main_8cpp_source.html", null ]
-];
--- doc/html/navtreeindex0.js
+++
@@ -1,8 +0,0 @@
-var NAVTREEINDEX0 =
-{
-"dir_68267d1309a1af8e8297ef4c3efbcdba.html":[0,0,0],
-"files.html":[0,0],
-"index.html":[],
-"main_8cpp_source.html":[0,0,0,0],
-"pages.html":[]
-};
